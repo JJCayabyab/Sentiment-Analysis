91591f794ed58fb1f313b125911cda99204e1e9b
jxsh2 <92442082+jxsh2@users.noreply.github.com>
2024-01-27 15:59:07 +0800

    Update main.py

---
+++ main.py
@@ -0,0 +1,5 @@
+# do not use any module or library
+# extraction of tweets using tweepy
+# clean the tweets using finite automata procedure
+# used regex to create scored based lexical to detect the good and bad words
+# used the score to be the output of the program
